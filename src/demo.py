7768b8d8fa923fc9bb272f4fd737530bd4a1af60
Owen Phillips <ophilli@clemson.edu>
2017-05-13 12:52:09 -0400

    Implement At Least 2 or More

--- src/demo.py
+++ src/demo.py
@@ -1,24 +1,25 @@
-# (Quora Background Info)[https://www.quora.com/Three-cards-are-pulled-from-a-deck-of-52-cards-What-is-the-probability-of-getting-at-least-one-club]
+# This program is designed to calculate sample probabilities for use in board
+# games. It is based on information from the following quora page
+# https://www.quora.com/Three-cards-are-pulled-from-a-deck-of-52-cards-What-is-the-probability-of-getting-at-least-one-club
 
 from scipy.special import comb
 
-def probFunc(deckSize, numCardsInCategory, numDraws):
+def probFunc(deckSize, numCardsInCategory, numAtLeast, numDraws):
     """
     Sums the combinations of all favorable events to calculate the probability
     of drawing at least one card in the category from the deck
     """
 
-    possibleEvents = comb(deckSize, numDraws)
-    favorableEvents = 0
-
-    if(numDraws <= 0):
+    if(numDraws < numAtLeast):
         return 0
-    if(numDraws == 1):
+    if(numDraws == numAtLeast):
         possibleEvents = deckSize
         favorableEvents = numCardsInCategory
 
     else:
-        for draws in range(numDraws):
+        possibleEvents = comb(deckSize, numDraws)
+        favorableEvents = 0
+        for draws in range(numAtLeast-1,numDraws):
             favorableEvents += comb(numCardsInCategory, draws+1) \
                 * comb(deckSize - numCardsInCategory, numDraws - draws - 1)
 
@@ -38,5 +39,19 @@ def probFromZero(deckSize, numCardsInCategory, numDraws):
     return 1 - (zeroEvents / possibleEvents)
 
 if __name__ == "__main__":
-    for x in range(0,53):
-        print(str(x) + ": " + str(probFromZero(52,4,x)))
+    choice = int(input('Specific # of draws (0) or full list (1)'))
+
+    deckSize = int(input('Enter the size of the deck: '))
+    numCardsInCategory = int(input('Enter the number of the cards in the category: '))
+    numAtLeast = int(input('Enter the number of cards to test for - P(X >= n): '))
+
+    if(choice == 0):
+        numDraws = int(input('Enter the number of card draws taken: '))
+
+        print(probFunc(deckSize, numCardsInCategory, numAtLeast, numDraws))
+
+
+    elif(choice == 1):
+        print("Draw | Probability")
+        for x in range(0,deckSize+1):
+            print(str(x) + ": " + str(probFunc(deckSize,numCardsInCategory,numAtLeast,x)))
--- src/test_demo.py
+++ src/test_demo.py
@@ -2,32 +2,52 @@ import demo
 from scipy.special import comb
 
 class TestDemos:
-    def test_drawCards_lte_0(self):
-        assert demo.probFunc(52,4,0) == 0
-        assert demo.probFunc(52,13,0) == 0
-        assert demo.probFunc(52,4,-1) == 0
-        assert demo.probFunc(52,13,-1) == 0
+    def test_drawCards_lt_numAtleast(self):
+        """
+        If the number of cards drawn is less than the number of of successes at
+        least necessary then we expect 0 probability
+        """
+        assert demo.probFunc(deckSize=52,numCardsInCategory=4,numAtLeast=1,numDraws=0) == 0
+        assert demo.probFunc(52,13,1,0) == 0
+        assert demo.probFunc(52,4,1,-1) == 0
+        assert demo.probFunc(52,13,1,-1) == 0
+        assert demo.probFunc(52,4,2,1) == 0
+        assert demo.probFunc(52,13,2,1) == 0
+        assert demo.probFunc(52,4,2,-1) == 0
+        assert demo.probFunc(52,13,2,-1) == 0
+
+    def test_at_least_more_than_1(self):
+        """ Compare our values against startrek.com/online-calculator-hypergeometric.aspx """
+        assert abs(demo.probFunc(52,13,1,10) - 0.959813879728687) < 1e-6
+        assert abs(demo.probFunc(52,13,2,10) - 0.785674025219665) < 1e-6
+        assert abs(demo.probFunc(52,13,3,10) - 0.482333633494272) < 1e-6
+        assert abs(demo.probFunc(52,13,4,10) - 0.204271607745995) < 1e-6
+        assert abs(demo.probFunc(52,13,5,10) - 0.0568144728794855) < 1e-6
+        assert abs(demo.probFunc(52,13,6,10) - 0.00997514768659347) < 1e-6
+        assert abs(demo.probFunc(52,13,7,10) - 0.0010533714593759) < 1e-6
+        # P(X >= 8) is ~= 6.2e-05 which is too small to bother checking
+
     def test_prob_three_draws_for_club_from_52_cards(self):
-        assert abs(demo.probFunc(52,13,3) - 0.58647058823529408) < 1e-6
+        assert abs(demo.probFunc(52,13,1,3) - 0.586470588235294) < 1e-6
 
     def test_prob_3_clubs_52_equals_1_minus_0_clubs_in_52(self):
-        assert abs(demo.probFunc(52,13,3) - (1 - comb(39,3) / comb(52, 3))) < 1e-6
+        assert abs(demo.probFunc(52,13,1,3) - (1 - comb(39,3) / comb(52, 3))) < 1e-6
 
     def test_prob_1_ace_in_52_1_draw(self):
-        assert abs(demo.probFunc(52,4,1) - 4/52) < 1e-6
+        assert abs(demo.probFunc(52,4,1,1) - 4/52) < 1e-6
 
     def test_prob_1_ace_52_equals_1_minus_0_ace_in_52(self):
-        assert abs(demo.probFunc(52,4,1) - (1 - comb(48,1) / comb(52,1))) < 1e-6
+        assert abs(demo.probFunc(52,4,1,1) - (1 - comb(48,1) / comb(52,1))) < 1e-6
 
     def test_probFunc_equals_probFromZero(self):
-        assert abs(demo.probFunc(52,4,1) - demo.probFromZero(52,4,1)) < 1e-6
-        assert abs(demo.probFunc(52,13,3) - demo.probFromZero(52,13,3)) < 1e-6
-        assert abs(demo.probFunc(52,4,52) - demo.probFromZero(52,3,52)) < 1e-6
-        assert abs(demo.probFunc(52,13,52) - demo.probFromZero(52,3,52)) < 1e-6
+        assert abs(demo.probFunc(52,4,1,1) - demo.probFromZero(52,4,1)) < 1e-6
+        assert abs(demo.probFunc(52,13,1,3) - demo.probFromZero(52,13,3)) < 1e-6
+        assert abs(demo.probFunc(52,4,1,52) - demo.probFromZero(52,3,52)) < 1e-6
+        assert abs(demo.probFunc(52,13,1,52) - demo.probFromZero(52,3,52)) < 1e-6
 
     def test_drawcards_equal_totalCards(self):
-        assert demo.probFunc(52,4,52) == 1
-        assert demo.probFunc(52,13,52) == 1
+        assert demo.probFunc(52,4,1,52) == 1
+        assert demo.probFunc(52,13,1,52) == 1
 
 class TestScipy:
     def test_comb(self):
